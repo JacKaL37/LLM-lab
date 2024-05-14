73217314dc735c65416f9f57ec6ae15c6ee1ff08
JacKaL37 <john.k.lindstedt@gmail.com>
2024-05-13 21:43:14 -0400

    test

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -174,7 +174,7 @@ export default {
       modelOptionsPre: ["gpt-4-0125-preview","gpt-4-1106-preview","gpt-3.5-turbo-1106",],
       modelOptions3: ["gpt-3.5-turbo","gpt-3.5-turbo-16k","gpt-3.5-turbo-instruct"],
       modelOptions4: ["gpt-4o","gpt-4-vision-preview","gpt-4-turbo-preview","gpt-4","gpt-4-32k"],
-      model: "gpt-4o",
+      model: "gpt-4o", 
       temperature: 0.7,
 
       currentAIresponse: { role: "ai", content: "", name: this.model, temp: this.temperature, prompt: this.prompts_id},
